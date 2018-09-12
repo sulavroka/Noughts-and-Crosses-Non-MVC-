4ff2ff222511fb2278cc977df8ca48e376b63220
Tekendra Roka <sulove_123@hotmail.com>
2018-09-12 11:44:12 +0100

    Noughts and Crosses Normal version

---
+++ Noughts and Crosses(Normal version)/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String[]args){
+		
+		Piece nought = new Piece(false);
+		
+		System.out.println(nought);
+	}
+	
+	
+	
+}
---
+++ Noughts and Crosses(Normal version)/src/Piece.java
@@ -0,0 +1,29 @@
+
+public class Piece {
+
+	private boolean isNought;
+	
+	public Piece(boolean isNought){
+		this.isNought = isNought;
+	}
+	
+	
+	// if the last move for example is nought then the next move will be cross
+	public Piece(Piece lastMove){
+		isNought = !lastMove.isNought;
+	}
+	
+	public String toString(){
+		if(isNought){
+			return "O";
+		}
+		else{
+			return "X";
+		}
+	}
+	
+	
+	
+	
+	
+}
